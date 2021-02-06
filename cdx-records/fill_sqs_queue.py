4782547091aef77d085553e8691f57b1fed6e085
MartineDeVos <mgdevos@gmail.com>
2021-02-06 15:47:30 +0100

    Send multiple custom messages to sqs

--- cdx-records/fill_sqs_queue.py
+++ cdx-records/fill_sqs_queue.py
@@ -1,46 +1,56 @@
 import boto3
 
-# Create SQS client
-sqs = boto3.client('sqs')
+def chunk_join(lst,n):
+    """Yield successive n-sized chunks from lst;
+       join elements in single string per chunk"""
+    for i in range(0, len(lst), n):
+        elements = lst[i:i + n]
+        yield ','.join(elements)
+
+def send_custom_message(queue_url,content,index=1):
+    """send message with custom content and attributes to sqs queue"""
+    
+    # Create SQS client
+    sqs = boto3.client('sqs')
+    
+    response = sqs.send_message(
+            QueueUrl=queue_url,
+            DelaySeconds=10,
+            MessageAttributes={
+                'Title': {
+                    'DataType': 'String',
+                    'StringValue': f'Message nr {index}'
+                },
+                'Author': {
+                    'DataType': 'String',
+                    'StringValue': 'mvos'
+                },
+            },
+            MessageBody=(content)
+            )
+    return response
 
 def main():
+    # TODO: replace by input from file
+    companies = [
+        "bitpanda.com",
+        "musictraveler.com",
+        "ikangai.com",
+        "medicus.ai",
+        "refurbed.de",
+        "usound.com",
+        "bikemap.net",
+        "bsurance.tech",
+        "checkyeti.com",
+        "parkbob.com"
+        ]
     queue_url = "https://sqs.eu-central-1.amazonaws.com/080708105962/crunchbase-dev-mvos-lambda-cdx-queue"
 
-    # Send message to SQS queue
-    response = sqs.send_message(
-        QueueUrl=queue_url,
-        DelaySeconds=10,
-        MessageAttributes={
-            'Title': {
-                'DataType': 'String',
-                'StringValue': 'First message'
-            },
-            'Author': {
-                'DataType': 'String',
-                'StringValue': 'mvos'
-            },
-        },
-        MessageBody=(
-            "bitpanda.com,"
-            "musictraveler.com,"
-            "ikangai.com,"
-            "medicus.ai,"
-            "refurbed.de,"
-            "usound.com,"
-            "bikemap.net,"
-            "bsurance.tech,"
-            "checkyeti.com,"
-            "parkbob.com,"
-            "getbyrd.com,"
-            "nuki.io,"
-            "blockpit.io,"
-            "morpher.com,"
-            "imagebiopsylab.ai,"
-            "getmimo.com"
-        )
-    )
-
-    print(response['MessageId'])
+    # Convert companies list into multiple message bodies;
+    # send corresponding messages to SQS queue
+    for i,cc in enumerate(chunk_join(companies,5)):
+        res = send_custom_message(queue_url,cc,i)
+        print(res['MessageId'])
 
 if __name__ == "__main__":
     main()
