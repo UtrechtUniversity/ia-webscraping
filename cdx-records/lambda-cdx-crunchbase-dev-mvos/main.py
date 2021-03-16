a48c844b1884b5522fd79a72762d93d27944743e
Robert Jan Bood <robert-jan.bood@surfsara.nl>
2021-03-16 19:53:02 +0100

    The following fixes are implemented: Function checks if the number of messages in the fetch queue is lower than the limit CDX api is now call asynchronously SQS messages are created in batches of 10 MessageDelay is increased by 10 seconds for every 30 messages send to SQS

--- cdx-records/lambda-cdx-crunchbase-dev-mvos/main.py
+++ cdx-records/lambda-cdx-crunchbase-dev-mvos/main.py
@@ -4,9 +4,20 @@ import json
 import os
 import re
 import requests
+import aiohttp
+import asyncio
+import time
+import logging
+import sys
 
 from urllib.parse import urlparse
 from urllib.error import URLError
+
+if os.environ.get('logging'):
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+else:
+    logging.basicConfig(stream=sys.stdout, level=logging.ERROR)
+logger = logging.getLogger()
 
 EXTENSIONS = [
     '.css','.js','.map','.xml','.png','.woff','.gif','.jpg', 'eot',
@@ -17,55 +28,32 @@ BLACKLIST = [
     re.compile(ext + '(\?|$)', re.IGNORECASE) for ext in EXTENSIONS
 ]
 
-BATCH_SIZE = 25
+TARGET_BUCKET = os.environ.get('target_bucket_id')
 
+# Define queues as SQS resource
+FETCH_SQS_LIMIT = 2000
+sqs = boto3.resource('sqs')
+fetch_sqs_queue = sqs.Queue(os.environ['sqs_fetch_id'])
+cdx_sqs_queue = sqs.Queue(os.environ['sqs_cdx_id'])
 
-def handler(event, context):
-    print("Started lambda-sqs example.")
-    sqs_receive_messages()
+def fetch_queue_limit_reached():
+    return int(fetch_sqs_queue.attributes.get('ApproximateNumberOfMessages')) > FETCH_SQS_LIMIT
 
-def sqs_receive_messages():
-    # Create SQS client
-    sqs = boto3.client('sqs')
-
-    queue_url = os.environ['sqs_cdx_id']
-
-    # Receive message from SQS queue
-    response = sqs.receive_message(
-        QueueUrl=queue_url,
+def get_cdx_sqs_messages():
+    response = cdx_sqs_queue.receive_messages(
         AttributeNames=[
             'SentTimestamp'
         ],
-        MaxNumberOfMessages=1,
+        MaxNumberOfMessages=10,
         MessageAttributeNames=[
             'All'
         ],
-        VisibilityTimeout=0,
-        WaitTimeSeconds=0
+        VisibilityTimeout=3,
+        WaitTimeSeconds=10
     )
 
-    if 'Messages' in response:
-        message = response['Messages'][0]
-        receipt_handle = message['ReceiptHandle']
-
-        handle_message(message)
-
-        # Delete received message from queue
-        sqs.delete_message(
-            QueueUrl=queue_url,
-            ReceiptHandle=receipt_handle
-        )
-    else:
-        message = 'None'
-    print(f'Received and Deleted message: {message}')
-
-def handle_message(message):
-    urls = message['Body'].split(',')
-    for url in urls:
-        domain = get_domain(url)
-        records = get_urls(domain)
-        if records:
-            sqs_send_urls(domain,records)
+    logger.info("Received '%d' sqs messages from cdx queue", len(response))
+    return response
 
 def get_domain(url):
     try:
@@ -73,11 +61,22 @@ def get_domain(url):
         domain = domain.replace("www.", "")
         return domain
     except URLError as e: 
-        print(f"Not a valid url:{e}")
+        logger.error("Not a valid url: %s", e)
         pass        
 
-def get_urls(domain):
-        payload = {
+async def get_urls_async(messages):
+    tasks=[]
+    async with aiohttp.ClientSession() as session:
+        for message in messages:
+            url = message.body
+            domain = get_domain(url)
+            task = asyncio.ensure_future(get_urls(message.message_id, message.receipt_handle, domain, session))
+            tasks.append(task)
+        task_results = await asyncio.gather(*tasks)
+    return task_results
+
+async def get_urls(sqs_message_id, sqs_receipt_handle, domain, session):
+    payload = {
             'url': domain,
             'matchType': 'prefix',
             'fl': 'urlkey,timestamp,digest',
@@ -92,58 +91,32 @@ def get_urls(domain):
             'output': 'json',
             'showResumeKey': 'true',
         }
-        filtered_urls = set()
 
-        # Get response from url
-        response = requests.get('http://web.archive.org/cdx/search/cdx', params=payload)
-        response_list = response.json()
+    async with session.get('http://web.archive.org/cdx/search/cdx', params=payload) as response:
+        response_list = await response.json()
 
-        # Extraction
-        if not response_list:
-            print(f"No records available: {domain}")
-            return None
+    # Extraction
+    if not response_list:
+        logger.info("No records available for '%s'", domain)
+        return {
+            "domain" : domain,
+            "urls" : None
+        }
 
-        header = response_list[0]
-        if not response_list[-2]:
-            resume_key = response_list[-1][0]
-            urls = response_list[1:-2]  # The before last one is always empty -> -2
-        else:
-            resume_key = "finished"
-            urls = response_list[1:]
+    header = response_list[0]
+    if not response_list[-2]:
+        resume_key = response_list[-1][0]
+        urls = response_list[1:-2]  # The before last one is always empty -> -2
+    else:
+        resume_key = "finished"
+        urls = response_list[1:]
 
-        return urls
-
-def sqs_send_message(content, delay_offset=0):
-    # Create SQS client
-    sqs = boto3.client('sqs')
-
-    queue_url = os.environ['sqs_fetch_id']
-    target_bucket = os.environ['target_bucket_id']
-
-    [url, timestamp] = content
-    file_name = (f'{url}.{timestamp}.txt').replace('/', '_')
-
-    body = {
-        'url': f'http://web.archive.org/web/{timestamp}/{url}',
-        'file_name': file_name,
-        'bucket_name': target_bucket
+    return {
+        "sqs_message_id" : sqs_message_id,
+        "sqs_receipt_handle" : sqs_receipt_handle,
+        "domain" : domain,
+        "urls" : urls
     }
-    response = sqs.send_message(
-        QueueUrl=queue_url,
-        DelaySeconds=10 + delay_offset,
-        MessageAttributes={
-                'Title': {
-                    'DataType': 'String',
-                    'StringValue': f'Message nr '
-                },
-                'Author': {
-                    'DataType': 'String',
-                    'StringValue': 'mvos'
-                },
-            },
-        MessageBody=json.dumps(body)
-    ) 
-    return response
 
 def restore_domain(domain,url):
     """Restore original domain name in CDX url"""
@@ -155,54 +128,160 @@ def restore_domain(domain,url):
     
     return new
 
-def chunks(L, n):
-    """ Yield successive n-sized chunks from L """
-    for i in range(0, len(L), n):
-        yield L[i:i+n]
-
-def sqs_send_urls(domain,records):
-    """Format cdx response and send in batches to sqs"""
-
+def filter_urls(domain, records):
     # Restore original domain in CDX url
     rec_list = [[restore_domain(domain,url),time,dgst] for url,time,dgst in records]   
     
-    # filter out unwanted urls and identical pages
+    ## filter out unwanted urls and identical pages
     rec_filtered = {}
     for [url, time, dgst] in rec_list:
         if dgst not in rec_filtered.keys() and \
             not any([bool(r.search(url)) for r in BLACKLIST]):
 
             rec_filtered[dgst] = [url, time]
+    return rec_filtered
 
-    # note the provided delay seconds, first 25 (BATCH_SIZE) have 
-    # a delaytime of 10, the next batch has a delay time of 20
-    for i, (_, rec) in enumerate(rec_filtered.items()):
-        response = sqs_send_message(rec, 10 + 10 *( i // BATCH_SIZE))
-        print(f"Sent message {response['MessageId']} to fetch queue")
+def send_urls_to_fetch_sqs_queue(urls):
+    delay_offset=0
+    messages_send=0
+    id = 0 # every message in a batch must have a unique ID
+    batch_messages = []
+    for i, (_, rec) in enumerate(urls.items()):
+        [url, timestamp] = rec
+        file_name = (f'{url}.{timestamp}.txt').replace('/', '_')
+              
+        if len(batch_messages) == 10:
+            sqs_send_message_batch(batch_messages)
+            messages_send += 10
+            if ((delay_offset < 900) and (messages_send % 30 == 0)):
+                # for every 30 messages send; increase message delay with 10 seconds
+                delay_offset += 10
+            batch_messages = []
+            
+        id += 1
+        batch_messages.append(
+            { 
+                'Id': str(id),
+                'MessageBody': json.dumps({
+                    'url': f'http://web.archive.org/web/{timestamp}/{url}',
+                    'file_name': file_name,
+                    'bucket_name': target_bucket
+                }),
+                'DelaySeconds': delay_offset,
+                'MessageAttributes': {
+                    'Title': {
+                        'DataType': 'String',
+                        'StringValue': f'Message nr '
+                    },
+                    'Author': {
+                        'DataType': 'String',
+                        'StringValue': 'mvos'
+                    },
+                }
+            }
+        )
+    #Send last messages
+    print(f"messages send: {messages_send + len(batch_messages)}")
+    if len(batch_messages) > 0:
+        sqs_send_message_batch(batch_messages)
+        batch_messages = []
+   
+
+def sqs_send_message_batch(messages):
+    response = fetch_sqs_queue.send_messages(
+        Entries=messages
+    )
+    if response.get('Failed'):
+        logger.warning("Failed to send the following messages to SQS: '%s'", str(response['Failed']))
+    else:
+        pass
+        #logger.info("Successfully send the following messages to SQS: '%s'", str(response['Successful']))
+
+def chunks(L, n):
+    """ Yield successive n-sized chunks from L """
+    for i in range(0, len(L), n):
+        yield L[i:i+n]
+
+def handler(event, context):
+    logger.info("Started CDX Lambda")
+
+    ## Check number of messages in Fetch queue
+    if fetch_queue_limit_reached():
+        logger.info("Number of messages in fetch sqs queue higher than limit of '%d'; early return", FETCH_SQS_LIMIT)
+        return
+
+    ## get messages from CDX Queue
+    messages = get_cdx_sqs_messages()
+
+    ## get Urls from internet archive (async)
+    task_results = asyncio.run(get_urls_async(messages))
+
+    ## Filter urls, send filtered urls to sqs
+    processed_messages = []
+    print(len(task_results))
+    for result in task_results:
+        if result['urls'] is None:
+            # No URLS found, continue with next one TODO: should we delete this message?
+            continue
+        print("number of urls found: " + str(len(result['urls'])))
+
+        ## Send filtered urls to fetch SQS queue
+        send_urls_to_fetch_sqs_queue(filter_urls(result['domain'], result['urls']))
+        processed_messages.append({
+            'Id': result['sqs_message_id'],
+            'ReceiptHandle': result['sqs_receipt_handle']
+        })
+
+    ## Delete processed SQS messages CDX queue in batches of 10
+    for proc_messages_batch in chunks(processed_messages, 10):
+        print("delete the following messages from cdx queue: " + str(proc_messages_batch))
+        cdx_sqs_queue.delete_messages(
+            Entries=proc_messages_batch
+        )
 
 def main():
     """Test script to run from command line"""
 
     message = (
-            "https://moonvision.io/,"
-            "http://www.enpulsion.com/,"
-            "https://kiweno.com,"
-            "https://rateboard.io,"
-            "https://www.meetfox.com,"
-            "https://etudo.co/?lang=en,"
-            "http://www.finnest.at,"
-            "http://www.journiapp.com,"
-            "https://www.prime-crowd.com/,"
-            "https://www.intellyo.com"
+        "http://www.usound.com/,"
+        "https://www.bikemap.net,"
+        "https://www.bsurance.tech,"
+        "https://www.checkyeti.com,"
+        "http://parkbob.com,"
+        "https://playerhunter.com/,"
+        "https://www.derbrutkasten.com,"
+        "https://www.oktav.com,"
+        "http://www.healcloud.com,"
+        "https://www.involve.me"
     )
         
     urls = message.split(',')
+    total_n_records = 0
+    message_id = 0
+    batch_messages = []
     for url in urls:
-        domain = get_domain(url)
-        records = get_urls(domain)
-        if records:
-            print(f"Records found for {domain}; sending to sqs")
-            sqs_send_urls(domain,records)
+        #send messages to sqs
+        message_id += 1
+        batch_messages.append(
+            { 
+                'Id': str(message_id),
+                'MessageBody': url,
+                'DelaySeconds': 0
+            }
+        )
+    cdx_sqs_queue.send_messages(
+        Entries=batch_messages
+    )
+    
+    handler(None, None)
+
+
 
 if __name__ == "__main__":
+    tic = time.perf_counter()
+    
     main()
+
+    toc = time.perf_counter()
+    print(f"Program runtime {toc - tic:0.4f} seconds")
+    
