93502601dff96f4a81fa8a41d5d26dfb2ec9e23c
MartineDeVos <mgdevos@gmail.com>
2021-04-20 16:42:44 +0200

    Send batch message to cdx queue

--- cdx-records/fill_sqs_queue.py
+++ cdx-records/fill_sqs_queue.py
@@ -3,43 +3,60 @@ import boto3
 import pandas as pd
 from pathlib import Path
 
-def chunk_join(lst,n):
-    """Yield successive n-sized chunks from lst;
-       join elements in single string per chunk"""
-    for i in range(0, len(lst), n):
-        try:
-            elements = lst[i:i + n]
-            yield ','.join(elements)
-        except:
-            pass
+def sqs_send_message_batch(messages,queue_url):
+    """send batch of messages to sqs queue"""
+    
+    # Define queue as SQS resource
+    sqs = boto3.resource('sqs')
+    queue = sqs.get_queue_by_name(QueueName=queue_url)
+        
+    response = queue.send_messages(Entries=messages)
+    
+    if response.get('Failed'):
+        print(f"Failed to send the following messages to SQS{str(response['Failed'])}")
+    else:
+        print(f"Message sent")
 
-def send_custom_message(queue_url,content,index=1):
-    """send message with custom content and attributes to sqs queue"""
+
+def send_urls_to_cdx_queue(companies,queue_url):
+    """Send ten messages to cdx sqs queue, one url per message"""
+    messages_send=0
+    batch_messages = []
+
+    for index,comp in enumerate(companies):
+
+        if len(batch_messages) == 10:
+            # Max batch size (N=10) reached; send batch
+            sqs_send_message_batch(batch_messages,queue_url)
+            batch_messages = []
+            messages_send += 10
+
+        batch_messages.append(
+            { 
+                'Id': str(index), # every message in a batch must have a unique ID; use index for this
+                'MessageBody': (comp),
+                'DelaySeconds': 10,
+                'MessageAttributes': {
+                    'Title': {
+                        'DataType': 'String',
+                        'StringValue': f'Message nr '
+                    },
+                    'Author': {
+                        'DataType': 'String',
+                        'StringValue': 'mvos'
+                    }
+                }
+            }
+        )
+    #Send last messages to SQS
+    if len(batch_messages) > 0 :
+        sqs_send_message_batch(batch_messages,queue_url)
     
-    # Create SQS client
-    sqs = boto3.client('sqs')
-    
-    response = sqs.send_message(
-            QueueUrl=queue_url,
-            DelaySeconds=10,
-            MessageAttributes={
-                'Title': {
-                    'DataType': 'String',
-                    'StringValue': f'Message nr {index}'
-                },
-                'Author': {
-                    'DataType': 'String',
-                    'StringValue': 'mvos'
-                },
-            },
-            MessageBody=(content)
-            )
-    return response
 
 def main():
     parser = argparse.ArgumentParser(description='Fill sqs queue with urls for which CDX records should be fetched')
     parser.add_argument("--queue", "-q", help="Enter sqs ID ")
-    parser.add_argument("--infile", "-f", help="Enter path to folder containing zipfiles")    
+    parser.add_argument("--infile", "-f", help="Enter path to file with urls")    
     args = parser.parse_args()
 
     boto3.setup_default_session(profile_name='crunch')             
@@ -52,11 +69,7 @@ def main():
 
     # Convert companies list into single message bodies;
     # Send corresponding messages to SQS queue
-    for i,comp in enumerate(companies):
-        res = send_custom_message(queue_url,comp,i)
-
-        print(f"Message ID: {res['MessageId']}")
-        print(f"Message : {res}")
-
+    send_urls_to_cdx_queue(companies,queue_url)
+    
 if __name__ == "__main__":
     main()
